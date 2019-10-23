46bb1dc0c5fb8a8d96ffe15dd961010487bfba95
TẤN NGUYỄN <tannguyen1107.bku@gmail.com>
2019-10-23 23:08:17 +0700

    initial commit

---
+++ Session2/index.js
@@ -0,0 +1,71 @@
+    //     // // let yob = prompt('enter your yob: ')
+    //     // // let age = 2019 - Number(yob)
+    //     // // console.log(age)
+    //     // // if(age < 16) {
+    //     // //     console.log('get out')
+    //     // // }
+    //     // // else if(age > 18) {
+    //     // //     console.log('bố mẹ cho em vào đây không')
+    //     // // }
+    //     // // else {
+    //     // //     console.log('WELLCOME')
+    //     // // }
+    //     // let random = Math.random(0, 100)
+    //     // let intrandom =Math.floor(random * 100)
+    //     // console.log(intrandom)
+    //     // if(intrandom < 30) {
+    //     //     console.log('rainny')
+    //     // }
+    //     // else if(intrandom < 60) {
+    //     //     console.log('cloundy')
+    //     // }
+    //     // else {
+    //     //     console.log('sunny')
+    //     // }
+    // // let menu = ['bún đậu', 'phở', 'cơm']
+    // // let newMonAn = 'com ga'
+    // // menu.push(newMonAn) // create
+    // // menu.push(newMonAn) // create
+    // // menu.push(newMonAn) // create
+    // // console.log(menu[1])
+    // // for(let i = 0; i < menu.length; i+=1) {
+    // //     console.log(menu[i]);
+    // // }
+    // // let n = menu.slice(0,2)
+    // // console.log(n);    
+    // // console.log(menu);
+    // // menu.splice(2, 1) //delete
+    // // console.log(menu);
+    // let n = prompt('xin chao! moi ban nhap lua chon:' ) // khoi tao (I)
+    // let menu = ['quan jean', 'ao thun', 'ao dai']
+    // if(n == 1)    // xem đồ -----> in ra món đồ (R)
+    // {
+    //     console.log(menu);  
+    // }
+    // else if(n == 2) // bán đồ ----> muốn bán gì (C)
+    // {
+    //  let ban = prompt('nhap do ban muon ban:')
+    //  menu.push(ban)
+    //  console.log(menu)
+    // }
+    // else if(n == 3) // mua đồ ---> mua đồ gì (D)
+    // {
+    // console.log(menu)
+    // let stt = prompt('nhap stt mon do ban muon mua')
+    // menu.splice(stt, 1)
+    // console.log(menu)
+    // }
+    // else if(n == 4) // đổi đồ ----> đổi đồ gì? update (U)
+    // {
+    //  console.log(menu)
+    //  let stt = prompt('nhap stt mon hang ban muon doi')
+    //  let doi = prompt('nhap ten mon hang muon doi')
+    //  menu[stt] = doi
+    //  console.log(menu)
+    // }
+    // else
+    // {
+    //     console.log('khong co lua chon cua ban, ban nen di cho khac choi')
+    // }
+    let n = Math.floor(1234/10)
+    console.log(n)
